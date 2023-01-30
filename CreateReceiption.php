91dade9a4652a0f040b15d44cefc50dafac4bb4c
unknown <Roseedee>
2023-01-30 18:49:19 +0700

    update complate

--- CreateReceiption.php
+++ CreateReceiption.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     session_start();
     if(!isset($_SESSION['login-state'])) {
         header('location: form_login.php');
@@ -6,12 +6,18 @@
     include('connection.php');
 
     $c_id = $_POST['customer-id'];
-    $e_id = $_POST['employee-id'];  
+    $e_id = $_POST['employee-id']; 
+    // echo $c_id;
+    // echo $e_id;
             
     $sql = "insert into receipt values (NULL, '$c_id', '$e_id', NULL, NULL);";
-    $con->query($sql);
-
-    $r_id = $con->insert_id;
-    $_SESSION['r_id_last'] = $r_id;
-    header('location: form_product.php?r_id='.$r_id.'')
+    if($con->query($sql)) {
+        $r_id = $con->insert_id;
+        // echo $r_id;
+        $_SESSION['r_id_last'] = $r_id;
+        header('location: form_product.php?r_id='.$r_id);
+        
+    }else {
+        echo $con->error;
+    }
 ?>
--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
     $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "miniproject_047_db";
+    $username = "id20223495_roseedee";
+    $password = "S-]8?fO(~\G[(Wl<";
+    $dbname = "id20223495_miniproject047";
 
     $con = new mysqli($host, $username, $password, $dbname);
 
